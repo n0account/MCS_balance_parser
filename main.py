4d5c7a27c92132ebb2b2e40b2cdd943f78a46940
alexey <alexey.pyhonin@gmail.com>
2023-07-18 11:53:41 +0400

    Изменил адрес, добавил csrf.

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import json
 
 auth_data = {'email': 'email', 'password': 'pass', 'app': 'ics',
              'device_id': 'device'}
+
 session = requests.session()
 response = session.post('https://mcs.mail.ru/api/v1/auth/signin', data=auth_data)
-parse_data = session.get('https://mcs.mail.ru/api/v1/projects/your_id/billing')
+session.headers.update({'x-csrf-token': 'csrf'})
+parse_data = session.get('https://mcs.mail.ru/iam/api/v2/projects/pid/billing/')
 soup = BeautifulSoup(parse_data.text, 'lxml').text
 balance_json = json.loads(soup)
-balance = (balance_json['balance']).split('.')[0]
-
+balance = (str(balance_json['balance'])).split('.')[0]
 print(balance)
